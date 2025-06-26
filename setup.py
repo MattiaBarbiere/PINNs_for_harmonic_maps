fbb1adfbf11c11447e58f1e17b953e150d3c45d7
MattiaBarbiere <mattia.barbiere@protonmail.com>
2025-06-26 22:35:42 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
 from setuptools import setup, find_packages
 
+# Read requirements from requirements.txt
+def read_requirements():
+    try:
+        with open('requirements.txt', 'r') as f:
+            return [line.strip() for line in f if line.strip() and not line.startswith('#')]
+    except FileNotFoundError:
+        return []
+
 setup(
-    name='hmpinn',       # Replace with your package name 
-    version='0.1',                  # Package version
-    author='Philipp Weder',             # Author name
-    author_email='philipp.weder@epfl.ch',  # Author email
-    description='PINN implementation for harmonic maps',  # Short description
-    long_description='TBD',  # Full description
-    long_description_content_type='text/markdown',  # Description content type
-    url='https://github.com/weder/hmpinnß',  # URL to your package repository
-    packages=find_packages(),       # Automatically find all packages in the directory
-    install_requires=[              # List of dependencies required to run your package
-        # Add more dependencies as needed
-    ],
-    classifiers=[                   # Classifiers to categorize your package (optional)
+    name='hmpinn',
+    version='0.1',
+    author='Mattia Barbiere',
+    author_email='mattia.barbiere@epfl.ch',
+    description='PINN implementation for harmonic maps',
+    long_description='Python package for solving the harmonic maps using Physics-Informed Neural Networks (PINNs).',
+    long_description_content_type='text/markdown',
+    url='https://github.com/MattiaBarbiere/PINNs_for_harmonic_maps/tree/main',
+    packages=find_packages(),
+    install_requires=read_requirements(),
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.11',
-        # Add more classifiers as needed
     ],
 )
